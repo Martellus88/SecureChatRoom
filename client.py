bc0f369bc60e9018aa2ccca8c7ff0891d47d0751
Marat <helvegen82@gmail.com>
2021-08-19 21:33:29 +0300

    Client.py finished!

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket
 import threading
 import tkinter as tk
-from tkinter import simpledialog
 from tkinter import scrolledtext
 from tkinter import messagebox
 
@@ -9,31 +8,22 @@ from tkinter import messagebox
 class Client:
 
     def __init__(self, ip: str, port: int):
-
         self.ip = ip
         self.port = port
 
-        self.window = None
         self.text_area = None
         self.text_input = None
+        self.user_area = None
 
         self.stop = False
 
-        msg = tk.Tk()
-        msg.withdraw()
-
-        self.user_name = simpledialog.askstring('Имя пользователя', 'Введите имя ')
+        self.window = tk.Tk()
+        self.window.withdraw()
 
     def exit_from_chat(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        if messagebox.askokcancel("Exit", "Do you want to exit the program?"):
             self.stop = True
             self.window.destroy()
-
-            print(self.listen_thread)
-            self.listen_thread.join()
-            print(self.listen_thread)
-
-            self.client_socket.shutdown(socket.SHUT_RDWR)
             self.client_socket.close()
             exit(0)
 
@@ -41,15 +31,17 @@ class Client:
         self.create_socket()
         self.init_window()
         self.init_text_area()
+        self.init_username_input()
         self.init_input_area()
 
-        self.listen_thread = threading.Thread(target=self.listen_server, daemon=True)  # TODO ?!
-        self.listen_thread.start()
+        listen_thread = threading.Thread(target=self.listen_server)
+        listen_thread.start()
 
-        # self.window.mainloop()
+        self.window.mainloop()
 
     def init_window(self):
-        self.window = tk.Tk()
+        self.window.deiconify()
+        self.window.title('Stay Safe Exile...')
         self.window.configure(bg='gray88')
         self.window.geometry('800x600')
         self.window.resizable(width=False, height=False)
@@ -59,42 +51,78 @@ class Client:
     def create_socket(self):
         try:
             self.client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-            self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.client_socket.connect((self.ip, self.port))
         except ConnectionRefusedError:
             print('Server is offline')
             exit(0)
 
     def init_text_area(self):
-        chat_label = tk.Label(self.window, text='Chat box:', bg='gray88')
-        chat_label.config(font=('Roboto', 15))
-        chat_label.pack(pady=3)
+        frame = tk.Frame(bg='gray88')
 
-        self.text_area = scrolledtext.ScrolledText(master=self.window, width=90, background='white smoke',
+        chat_label = tk.Label(frame, text='Chat box:', bg='gray88')
+        chat_label.config(font=('Roboto', 15))
+        chat_label.pack(side='top', anchor='w', padx=10)
+
+        self.text_area = scrolledtext.ScrolledText(master=frame, width=90, background='white smoke',
                                                    font=("Roboto", 11))
-        self.text_area.pack(side='top', padx=20, pady=5)
+        self.text_area.pack(side='left', padx=10)
         self.text_area.config(state='disabled')
 
-    def init_input_area(self):
-        label_input = tk.Label(master=self.window, text='Enter your message: ', bg='gray88')
-        label_input.config(font=('Roboto', 15))
-        label_input.pack(pady=5)
+        frame.pack(side='top')
 
-        self.text_input = tk.Text(master=self.window, width=80, height=10, background='white smoke',
+    def init_input_area(self):
+        frame = tk.Frame(bg='gray88')
+
+        label_input = tk.Label(master=frame, text='Enter your message: ', bg='gray88')
+        label_input.config(font=('Roboto', 14))
+        label_input.pack(side='top', anchor='w')
+
+        self.text_input = tk.Text(master=frame, width=80, height=5, background='white smoke',
                                   font=("Roboto", 10))
-        self.text_input.pack(side='bottom', padx=10, pady=13)
-        self.text_input.focus_set()
+        self.text_input.pack(side='left', pady=10)
 
         self.text_input.bind('<Return>', func=self.send_to_server)
         self.text_input.bind('<Shift-Return>', func=lambda event: self.text_input.insert(tk.END, ''))
 
+        frame.pack(side='top')
+
+    def init_username_input(self):
+        frame = tk.Frame(bg='gray88')
+
+        chat_label = tk.Label(frame, text='username:', bg='gray88')
+        chat_label.config(font=('Roboto', 12))
+        chat_label.pack(side='left', padx=15)
+
+        self.user_area = tk.Entry(master=frame, width=20, background='white smoke',
+                                  font=("Roboto", 11))
+        self.user_area.pack(side='left', anchor='nw', pady=10)
+
+        join_button = tk.Button(frame, text="join", width=5, command=self.join_to_server, bd=3)
+        join_button.pack(side='left', padx=10)
+
+        self.user_area.focus_set()
+
+        frame.pack(side='top', anchor='n')
+
+    def join_to_server(self):
+        if len(self.user_area.get()) == 0:
+            messagebox.showerror('Enter nickname', 'Please enter a nickname')
+            return
+        self.user_area.config(state='disable')
+        self.user_name = self.user_area.get()
+        self.client_socket.send(self.user_name.encode('utf-8'))
+
     def send_to_server(self, event):
+        if len(self.user_area.get()) == 0:
+            messagebox.showerror('Enter nickname', 'Please enter a nickname')
+            return
+
         msg = self.text_input.get('1.0', tk.END).strip()
 
         if msg:
             self.text_input.delete('0.0', tk.END)
             self.text_area.config(state='normal')
-            self.text_area.insert(tk.END, 'Вы: ' + msg + '\n')
+            self.text_area.insert(tk.END, 'You: ' + msg + '\n')
             self.text_area.yview(tk.END)
             self.client_socket.sendall(msg.encode('utf-8'))
             self.text_area.config(state='disabled')
@@ -104,15 +132,13 @@ class Client:
             try:
                 serv_msg = self.client_socket.recv(1024)
 
-                if serv_msg.decode('utf-8') == 'username':
-                    self.client_socket.sendall(self.user_name.encode('utf-8'))
-                else:
-                    self.text_area.config(state='normal')
-                    self.text_area.insert(tk.END, serv_msg.decode('utf-8'))
-                    self.text_area.yview(tk.END)
-                    self.text_area.config(state='disabled')
-            # except ConnectionRefusedError as e:
-            #     print(type(e))
+                self.text_area.config(state='normal')
+                self.text_area.insert(tk.END, serv_msg.decode('utf-8'))
+                self.text_area.yview(tk.END)
+                self.text_area.config(state='disabled')
+
+            except ConnectionAbortedError as e:
+                print(e, type(e))
             except ConnectionResetError:
                 print('Server is offline')
 
@@ -120,4 +146,3 @@ class Client:
 if __name__ == '__main__':
     client = Client('127.0.0.1', 6666)
     client.run()
-    client.window.mainloop()
